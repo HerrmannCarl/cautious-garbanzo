6d16cbb97a40083d1fcf426ba106456c188401ac
Carl Herrmann <herrmann.carl@gmail.com>
2020-08-26 12:05:11 -0400

    Add Database Initialization to main page

--- Captions/captionScripts.js
+++ Captions/captionScripts.js
@@ -1,6 +1,6 @@
 function genDBs(result_tag) {
   var xhttp = new XMLHttpRequest();
-  document.getElementById("button_1").style = "background-color:green" ;
+  document.getElementById(result_tag).style = "background-color:lightgreen" ;
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
      document.getElementById(result_tag).innerHTML = "Database initilized";
