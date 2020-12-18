da7c00b8e603a9dd58a1c1833f13cd8e62586bd9
Carl Herrmann <herrmann.carl@gmail.com>
2020-12-18 01:27:06 +0100

    Functional version of website with trips

---
+++ Captions/dayInfo.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "db5000775650.hosting-data.io";
+$username = "dbu910176";
+$password = "ThisIsAWeakPassword!1";
+$dbname = "dbs701200";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Get the requested image / caption fromthe URL
+$id = $_REQUEST["id"];
+
+$query1 = "SELECT * FROM Days WHERE id = (?)";
+
+$stmt = $conn->prepare($query1);
+$stmt->bind_param("i", $id);
+$stmt->execute();
+
+$result1 = $stmt->get_result();
+$row1 = $result1->fetch_assoc();
+
+$output = array("id"=>$row1["id"],"create_date"=>$row1["create_date"],"tdate"=>$row1["tdate"],"startLocation"=>$row1["startLocation"],"finishLocation"=>$row1["finishLocation"],"photos"=>$row1["photos"],"description"=>$row1["description"],"header"=>$row1["header"]);
+$output_JSON = json_encode($output);
+echo $output_JSON;
+
+$stmt->close();
+$conn->close();
+?>
